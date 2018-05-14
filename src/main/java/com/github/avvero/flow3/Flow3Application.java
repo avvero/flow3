4a7a15b35d0436a5b842c46668927ae6fcf2f995
belyaev-ay <fxdev-belyaev-ay@fxclub.org>
2018-05-14 08:44:55 +0700

    Init with resources

---
+++ src/main/java/com/github/avvero/flow3/Flow3Application.java
@@ -0,0 +1,12 @@
+package com.github.avvero.flow3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Flow3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Flow3Application.class, args);
+	}
+}
